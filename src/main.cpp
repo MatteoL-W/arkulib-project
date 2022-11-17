3176f3b0f4ac98baddd6b3d243ac28d8b18299b0
Massa Elise <elise.massa.perso@gmail.com>
2022-11-17 18:32:11 +0100

    new test in the main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
+#include <iostream>
+#include "Rational.hpp"
+
 int main() {
+    Arkulib::Rational d1 = Arkulib::Rational(10, 7);
+    std::cout << d1;
+    Arkulib::Rational d2 = Arkulib::Rational(2, 3);
+    Arkulib::Rational d3 = d1 + d2;
+    std::cout << d3 << std::endl;
+    d3 = d1 * d2;
+    std::cout << d3<< std::endl;
+    d3 = d1 / d2;
+    std::cout << d3<< std::endl;
+    d3 = d1.inverse();
+    std::cout << d3<< std::endl;
     return 0;
 }
