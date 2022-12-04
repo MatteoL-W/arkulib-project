58ecf109b5dcb7954373621ba306f2129799fbd3
MatteoL-petitordi <matteo.ftt@gmail.com>
2022-12-04 17:46:17 +0100

    Adding suite example in the project

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,27 @@
 #include "Rational.hpp"
 
 int main() {
-    Arkulib::Rational d1 = Arkulib::Rational(10, 7);
-    std::cout << d1;
-    Arkulib::Rational d2 = Arkulib::Rational(2, 3);
-    Arkulib::Rational d3 = d1 + d2;
-    std::cout << d3 << std::endl;
-    d3 = d1 * d2;
-    std::cout << d3<< std::endl;
-    d3 = d1 / d2;
-    std::cout << d3<< std::endl;
-    d3 = d1.inverse();
-    std::cout << d3<< std::endl;
-    return 0;
+    Arkulib::Rational arkulibUn{1, 3};
+    double doubleUn = 1. / 3.;
+
+    int n = 0;
+    std::cout << "u[n+1] = 4 * u[n] - 1" << std::endl << std::endl;
+
+    std::cout << "The suite is constant with the value 1/3." << std::endl;
+    std::cout << "It diverges blatantly from 20 iterations with the double." << std::endl << std::endl;
+
+    std::cout << "Give the number of iteration you want to check with a double and with the Arkulib Rationals (n of u[n])" << std::endl;
+    std::cout << "Number of iterations : " << std::endl;
+    std::cin >> n;
+
+    auto suiteWithDouble = [](const double un){return 4 * un - 1;};
+    auto suiteWithArkulibRationals = [](const Arkulib::Rational<int> un){return 4 * un - 1;};
+
+    for (int i = 1; i < n; i++) {
+        arkulibUn = suiteWithArkulibRationals(arkulibUn);
+        doubleUn = suiteWithDouble(doubleUn);
+    }
+
+    std::cout << "Arkulib Rational solution : " << arkulibUn << std::endl;
+    std::cout << "double solution : " << doubleUn << std::endl;
 }
